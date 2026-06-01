692b065651f9c2e468b28b7c51140e010639e32a
Emily Phelps <52965134+EmilyPhelps@users.noreply.github.com>
2026-06-01 18:51:33 +0100

    Update and rename data_setup.R to setup/data_setup.R

--- setup/data_setup.R
+++ setup/data_setup.R
@@ -15,13 +15,4 @@ gld$temperature_st<-scale(gld$temperature, scale=TRUE)
 #Replace missing values with mean after scaling
 gld <- gld %>% mutate(temperature_st = coalesce(as.numeric(temperature_st), 0))
 
-## Cichlid data
-gld_cp <-read.csv(paste0(path, "data/Scototaxis_cichlid_pilot.csv"), 
-                  header=T)
-gld_cp <- gld_cp[1:59,]
-#define a preference measure 
-gld_cp$preference<-gld_cp$TimeLight-(500-gld_cp$TimeLight) #preferce score, 0 means equal time in light and dark
 
-gld_cp$age_st<-scale(gld_cp$day.age., scale=FALSE) 
-
-gld_cp$temperature_st<-scale(gld_cp$Temp, scale=TRUE)  
